b9fe63e9b05f48a0b1e0fe4c998f080dee2dcccd
Tayná Porfiro <74004654+TaynaPorfiro@users.noreply.github.com>
2020-11-26 15:51:50 -0300

    Upload do Exercício de Função

---
+++ Função - Exercício MultDiv.java
@@ -0,0 +1,12 @@
+class MultDiv {
+	public static int multiplicacao(int num1, int num2) {
+		return (num1*num2);
+	}
+	public static int divisao(int num1, int num2) {
+		return (num1/num2);
+	}
+	public static void main (String[]args) {
+		System.out.println(MultDiv.multiplicacao(30, 6));
+		System.out.println(MultDiv.divisao(1550, 10));
+	}
+}
